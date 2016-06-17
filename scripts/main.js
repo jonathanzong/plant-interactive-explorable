5551fa1dcbe80a259b9f71263a1302b2ff649fc1
Jonathan Zong <jonathan@jonathanzong.com>
2016-06-17 01:09:40 -0400

    proof of concept

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+$(function () {
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  });
+
+  // get all rows
+  var rows = document.querySelectorAll(".row");
+
+  // create scene for every slide
+  for (var i=0; i<rows.length; i++) {
+    // new ScrollMagic.Scene({
+    //     triggerElement: rows[i]
+    //   })
+    //   .setPin(rows[i])
+    //   .addTo(controller);
+    //
+
+    new ScrollMagic.Scene({
+        triggerElement: rows[i],
+        duration: $(rows[i]).height()
+      })
+      .setPin($(rows[i]).find('.pinnable').get(0))
+      .addTo(controller);
+    console.log('hi');
+
+  }
+
+  $('.pinnable p').hover(function() {
+    $(this).text("THANK YOU");
+  }, function() {
+    $(this).text("HELLO PLEASE HOVER OVER ME");
+  });
+});

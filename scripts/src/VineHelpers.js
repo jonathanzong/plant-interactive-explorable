ea75584ab288614eb0a4dc67495ee86acecb7c1d
Jonathan Zong <jonathan@jonathanzong.com>
2016-09-01 23:33:28 -0500

    shadows

--- scripts/src/VineHelpers.js
+++ scripts/src/VineHelpers.js
@@ -18,19 +18,28 @@ var VineScene = function(opts) {
   var vines = [];
 
   var vineTexture = PIXI.Texture.fromImage(opts.textureImage || 'images/vine.png');
+  var shadowTexture = PIXI.Texture.fromImage('images/vine-shadow.png');
 
   function addVine() {
     var points = [];
+    var points2 = [];
     for (var i = 0; i < pointCount; i++) {
       points.push(new PIXI.Point(i * ropeLength / pointCount, 0));
+      points2.push(new PIXI.Point(i * ropeLength / pointCount, 0));
     }
     var vine = new PIXI.mesh.Rope(vineTexture, points);
     vine.scale.x = 0.5;
     vine.scale.y = 0.5;
+    var shadow = new PIXI.mesh.Rope(shadowTexture, points2);
+    shadow.scale.x = 0.5;
+    shadow.scale.y = 0.5;
     vines.push({
       vine: vine,
-      points: points
+      shadow: shadow,
+      points: points,
+      points2: points2,
     })
+    stage.addChild(shadow);
     stage.addChild(vine);
   }
 
@@ -41,6 +50,11 @@ var VineScene = function(opts) {
 
   function updatePoints(i, fun) {
     fun(vines[i].points);
+    fun(vines[i].points2);
+    for (var j = 0; j < vines[i].points2.length; j++) {
+      vines[i].points2[j].x = vines[i].points2[j].x + 5;
+      vines[i].points2[j].y = vines[i].points2[j].y + 5;
+    };
   }
 
 
